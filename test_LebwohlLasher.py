2801572880e820315cd463d8ba4eebb63f27d248
Emily Wright <km20200@bc4login1.bc4.acrc.priv>
2023-11-01 12:01:57 +0000

    finished writing pytest

--- test_LebwohlLasher.py
+++ test_LebwohlLasher.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pytest
-from LebwohlLasher import initdat, one_energy
+from LebwohlLasher import *
 
-@pytest.mark.parametrize("nmax", [1, 5, 10, 100])  # Test with different nmax values
-def initdat(nmax):
+@pytest.mark.parametrize("nmax", [1, 5, 10, 25, 100])  # Test with different nmax values
+def test_initdat(nmax):
     
     arr = initdat(nmax)
 
@@ -15,11 +15,44 @@ def initdat(nmax):
     assert np.all(0 <= arr) and np.all(arr <= 2 * np.pi)
 
 
-def test_one_energy(ix, iy, nmax, expected_energy):
+def test_one_energy():
+    # initialise arbitary size lattice and random single cell
+    nmax = 25
+    np.random.seed(42)
+    arr = np.random.random_sample((nmax, nmax))*2.0*np.pi
+    ix, iy = 1, 1
+    
+    # check output against true value
+    en = one_energy(arr, ix, iy, nmax)
+    assert en == -0.5281352876995391
 
-def test_all_energy(arr,nmax):
+def test_all_energy():
+    # initialise arbitary size lattice
+    nmax = 25
+    np.random.seed(42)
+    arr = np.random.random_sample((nmax, nmax))*2.0*np.pi
+    
+    # check output against true value
+    enall = all_energy(arr, nmax)
+    assert enall == -683.6831640661935
 
-def test_get_order(arr,nmax):
+def test_get_order():
+    # initialise arbitary size lattice
+    nmax = 25
+    np.random.seed(42)
+    arr = np.random.random_sample((nmax, nmax))*2.0*np.pi
 
-def test_MC_step(arr,Ts,nmax):
+    # check output against true value
+    order = get_order(arr, nmax)
+    assert order == 0.2815966805248449
 
+def test_MC_step():
+    # initialise arbitary size lattice and arbitrary reduced temp
+    nmax = 25
+    np.random.seed(42)
+    arr = np.random.random_sample((nmax, nmax))*2.0*np.pi
+    Ts = 0.5
+
+    # testing function output against calculated true value
+    MC = MC_step(arr, Ts, nmax)
+    assert MC >= 0 and MC <= 1
